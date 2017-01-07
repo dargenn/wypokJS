3bb1cc3581cc1c3de607864cacbf4c4fe3389c72
dargenn <dargenn@o2.pl>
2017-01-07 21:07:27 +0100

    Inicjalizacja projektu

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,53 @@
+var app = angular.module('wypok', ['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider.state('home', {
+		url : '/home',
+		templateUrl : 'templates/home.html',
+		controller : 'MainController',
+		resolve : {
+			postPromise : ['posts',
+			function(posts) {
+				return posts.getAll();
+			}]
+
+		}
+	}).state('posts', {
+		url : '/posts/:id',
+		templateUrl : 'templates/posts.html',
+		controller : 'PostsController',
+		resolve : {
+			post : ['$stateParams', 'posts',
+			function($stateParams, posts) {
+				return posts.get($stateParams.id);
+			}]
+
+		}
+	}).state('login', {
+		url : '/login',
+		templateUrl : 'templates/login.html',
+		controller : 'AuthenticationController',
+		onEnter : ['$state', 'auth',
+		function($state, auth) {
+			if (auth.isLoggedIn()) {
+				$state.go('home');
+			}
+		}]
+
+	}).state('register', {
+		url : '/register',
+		templateUrl : 'templates/register.html',
+		controller : 'AuthenticationController',
+		onEnter : ['$state', 'auth',
+		function($state, auth) {
+			if (auth.isLoggedIn()) {
+				$state.go('home');
+			}
+		}]
+	});
+
+	$urlRouterProvider.otherwise('home');
+}]);
+
---
+++ public/javascripts/controller/NavigationController.js
@@ -0,0 +1,5 @@
+angular.module('wypok').controller('NavigationController', ['$scope', 'auth', function ($scope, auth) {
+    $scope.isLoggedIn = auth.isLoggedIn;
+    $scope.currentUser = auth.currentUser;
+    $scope.logOut = auth.logOut;
+}]);
